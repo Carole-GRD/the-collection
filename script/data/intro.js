3f568fce266badf7d9d0d4acf7f7a75abe45a23a
Carole Gérard <gerard.carole78@gmail.com>
2023-09-21 18:07:39 +0200

    url images youtube et logo

--- script/data/intro.js
+++ script/data/intro.js
@@ -1,5 +1,5 @@
 const IntroData = {
-    logo: "../../image/logo.jpg",
+    logo: "./image/logo.jpg",
     title: "Collection de lépidoptères",
     text1: "Le papillon ou lépidoptère est un ordre d\'insectes, une fois adulte, il possède des ailes qui lui permettent de voler.",
     text2: "Avant d\'avoir des ailes, les papillons apparaissent sous forme de larves, appelées chenilles, qui ont un corps allongé, parfois lisse ou d\'autres fois velu. Les chenilles possèdent trois paires de pattes, plus parfois des « fausses pattes » appelées ventouses. La chenille passe tout son temps à manger, surtout des feuilles d\'arbre et d\'arbustes, pour se faire des réserves de nourriture. Elles ne mangent que des feuilles et sont donc herbivores. Pour devenir des « papillons adultes », la chenille s\'entoure d\'un cocon, une enveloppe formée par un long fil de soie. À l\'intérieur de ce cocon va se produire la métamorphose, c\'est-à-dire le changement de forme de la chenille vers une chrysalide puis en un papillon. Certaines chenilles deviennent des chrysalides sans se protéger dans un cocon, elles s\'accrochent juste la tête en bas et leur peau s\'ouvre pour laisser apparaître la chrysalide."
